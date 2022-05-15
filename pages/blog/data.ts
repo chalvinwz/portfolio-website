c8c622b2584763ae9f9e1e8b08af8625519dc873
Chalvin Wiradhika <chalvinwz@gmail.com>
2022-05-15 19:02:08 +0700

    Add a link to a single blog

---
+++ pages/blog/data.ts
@@ -0,0 +1,16 @@
+type Links = {
+	links: { link: string; label: string }[]
+}
+
+export const data: Links = {
+	links: [
+		{
+			link: '/blog/introduction',
+			label: 'Introduction',
+		},
+		{
+			link: '/blog/my-second-page',
+			label: 'My second page',
+		},
+	],
+}
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,11 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
 
+import { data } from './data'
+
 import Layout from '../../components/Layout'
+import { Anchor, Group } from '@mantine/core'
 
 const Blog = () => {
 	return (
 		<Layout title='Blog - Chalvin Wiradhika'>
-			<div>Blog</div>
+			<Group spacing={20}>
+				{data.links.map((link) => (
+					<Link href={link.link} key={link.label} passHref>
+						<Anchor>{link.label}</Anchor>
+					</Link>
+				))}
+			</Group>
 		</Layout>
 	)
 }
