986346e93ad63b347dc28fd4ace891b24de75ab7
Chalvin Wiradhika <chalvinwz@gmail.com>
2022-05-15 18:34:48 +0700

    Update next.config.js page extensions

--- next.config.js
+++ next.config.js
@@ -9,5 +9,5 @@ const withMDX = require('@next/mdx')({
 })
 module.exports = withMDX({
 	// Append the default value with md extensions
-	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+	pageExtensions: ['tsx', 'mdx'],
 })
